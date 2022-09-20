277d96c613d84e06cbf7761e77075558608e2e8e
Nataliya Zaytseva <N.Zaitseva@avelar-solar.com>
2022-09-20 17:37:58 +0300

    dz 002 max из трех чисел

---
+++ dz_002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number1 > max)
+{
+    max = number1;
+    Console.Write("max= ");
+    Console.WriteLine(max);
+}
+if (number2 > max)
+{
+    max = number2;
+    Console.Write("max= ");
+    Console.WriteLine(max);
+}
+if (number3 > max)
+{
+    max = number3;
+    Console.Write("max= ");
+    Console.WriteLine(max);
+}

305049e504944367910716f2021f83c4d039864b
Nataliya Zaytseva <N.Zaitseva@avelar-solar.com>
2022-09-20 17:15:49 +0300

    dz 001 max/min

---
+++ dz_001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.WriteLine("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("max=");
+    Console.WriteLine(number1);
+    Console.Write("min=");
+    Console.WriteLine(number2);
+}
+else if (number1 < number2)
+{
+    Console.Write("max=");
+    Console.WriteLine(number2);
+    Console.Write("min=");
+    Console.WriteLine(number1);
+}

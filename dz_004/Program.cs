f7520ad3595ed2db492bf2b23eb5f4fda1f9e8e6
Nataliya Zaytseva <N.Zaitseva@avelar-solar.com>
2022-09-21 12:02:06 +0300

    dz 004 четные числа от 1 до n

---
+++ dz_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// задача на цикл
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 2;
+while (counter <= number)
+{
+    Console.WriteLine(counter);
+    counter=counter + 2;
+}

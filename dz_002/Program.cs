71458cd6309743cd39210733ca0f245e6f163ce6
Nataliya Zaytseva <N.Zaitseva@avelar-solar.com>
2022-09-21 12:12:42 +0300

    dz 002 max из 3-х чисел

--- dz_002/Program.cs
+++ dz_002/Program.cs
@@ -4,31 +4,26 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Clear();
+
 Console.WriteLine("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+int number1 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите третье число: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
 
 int max = number1;
-if (number1 > max)
-{
-    max = number1;
-    Console.Write("max= ");
-    Console.WriteLine(max);
-}
+
 if (number2 > max)
 {
     max = number2;
-    Console.Write("max= ");
-    Console.WriteLine(max);
 }
 if (number3 > max)
 {
     max = number3;
-    Console.Write("max= ");
-    Console.WriteLine(max);
 }
+Console.Write("max = ");
+Console.WriteLine(max);
